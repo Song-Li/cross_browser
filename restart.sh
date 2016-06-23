6a45002bd46e2d07e98dda4ce210b3374405d8be
songli <993793072@qq.com>
2016-06-23 19:35:55 +0000

    changed style of getid

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 python wipe_table.py cross_browser new_data uid;
 cp -r /home/site/data /home/site/databk;
-sudo rm -r /home/site/data/images/origins/* ;
+find /home/site/data/images/origins/ -name "*.*" -delete
 sudo rm -r /home/site/data/images/generated/* ;
 sudo /etc/init.d/apache2 restart
