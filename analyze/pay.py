204bb91fb9d15fd988cd019ac73d79fe9d3fad65
SongLi <993793072@qq.com>
2016-07-12 20:59:41 +0000

    make pay.py work

--- analyze/pay.py
+++ analyze/pay.py
@@ -2,19 +2,6 @@ from boto.mturk.connection import MTurkConnection
 from boto.mturk.price import Price
 import ConfigParser
 
-config = ConfigParser.ConfigParser()
-config.read("./keys.ignore")
-ACCESS_ID = config.get('keys', 'ACCESS_ID')
-SECRET_KEY = config.get('keys', 'SECRET_KEY')
-
-
-HOST = 'mechanicalturk.amazonaws.com'
-
-mturk = MTurkConnection(aws_access_key_id=ACCESS_ID,
-        aws_secret_access_key=SECRET_KEY,
-        host=HOST)
-hits = mturk.get_all_hits()
-
 def decode(code):
     mapping = ['D','E','F','B','G','M','N','A','I','L']
     res = ""
@@ -28,16 +15,40 @@ def decode(code):
             res += '_'
     return res
 
-def bonus(workerId, assignmentId):
+def bonus(mturk, price, workerId, assignmentId):
+    mturk.grant_bonus(workerId, assignmentId, price, 'Thank you for finishing three browsers!')
+    mturk.approve_assignment(assignmentId, feedback="Thank you")
     print 'Bonus', workerId
-def approve(workerId, assignmentId):
-    print 'approve', workerId
-def reject(workerId, assignmentId):
-    print 'reject', workerId
+
+def approve(mturk, workerId, assignmentId):
+    mturk.approve_assignment(assignmentId, feedback="Thank you")
+    print 'approved', workerId
+
+def reject(mturk, workerId, assignmentId):
+    mturk.reject_assignment(assignmentId, feedback="Not the right code or you have already done the same task!")
+    print 'rejected', workerId
+
+
+
+config = ConfigParser.ConfigParser()
+config.read("./keys.ignore")
+ACCESS_ID = config.get('keys', 'ACCESS_ID')
+SECRET_KEY = config.get('keys', 'SECRET_KEY')
+price = Price(0.01)
+
+
+HOST = 'mechanicalturk.amazonaws.com'
+
+mturk = MTurkConnection(aws_access_key_id=ACCESS_ID,
+        aws_secret_access_key=SECRET_KEY,
+        host=HOST)
+hits = mturk.get_all_hits()
+
 
 for hit in hits:
     print "hit ID: ", hit.HITId
-    assignments = mturk.get_assignments(hit.HITId, page_size=100)
+    assignments = mturk.get_assignments(hit.HITId, status="Submitted", page_size=100)
+    #assignments = mturk.get_assignments(hit.HITId, page_size=100)
     print len(assignments)
     for assignment in assignments:
         print assignment.WorkerId
@@ -49,10 +60,10 @@ for hit in hits:
 
         if code != '-1':
             if(code.find('_') != -1):
-                bonus(assignment.WorkerId, assignment.AssignmentId)
+                bonus(mturk, price, assignment.WorkerId, assignment.AssignmentId)
             else:
-                approve(assignment.WorkerId, assignment.AssignmentId)
+                approve(mturk, assignment.WorkerId, assignment.AssignmentId)
         else:
-            reject(assignment.WorkerId, assignment.AssignmentId)
+            reject(mturk, assignment.WorkerId, assignment.AssignmentId)
 
 
