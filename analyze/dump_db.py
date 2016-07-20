2c97ff8beb7c94bc4f36638ecc8bb057d47326e1
Erik Wijmans <erik@ip-172-31-11-180.ec2.internal>
2016-07-20 20:23:03 +0000

    xfer

--- analyze/dump_db.py
+++ analyze/dump_db.py
@@ -8,12 +8,17 @@ table_name = "round_2_data"
 db = MySQLdb.connect("localhost", "erik", "erik", db_name)
 cursor = db.cursor()
 
-cursor.execute("SELECT browser, hashes FROM {}".format(table_name))
+cursor.execute("SELECT DISTINCT(user_id) FROM {}".format(table_name))
+uids = [x for x, in cursor.fetchall()]
 f = open("hashes.txt", "w")
-for browser, h in cursor.fetchall():
-  hashes = h.split("&")
-  f.write(("{} " * (1 + len(hashes))).format(browser, *hashes))
-  f.write("\n")
+for uid in uids:
+  cursor.execute("SELECT browser, hashes FROM {} where user_id='{}'".format(table_name, uid))
+  data = cursor.fetchall()
+  f.write("{}\n".format(len(data)))
+  for browser, h in data:
+    hashes = h.split("&")
+    f.write(("{} " * (1 + len(hashes))).format(browser, *hashes))
+    f.write("\n")
 
 f.flush()
 f.close()
