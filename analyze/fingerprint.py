36cc396d5aee8a9f90eeeca166399cd0e33b1066
SongLi <993793072@qq.com>
2016-08-08 20:25:22 +0000

    temp

--- analyze/fingerprint.py
+++ analyze/fingerprint.py
@@ -2,49 +2,40 @@
 
 from enum import Enum
 from sets import Set
+from json import loads
+
+class Feature_Lists(Enum):
+  Mapped_All="agent, timezone, resolution, fontlist (Flash), plugins, cookies enabled, localstorage enabled, acceptable formats, encoding, headerkeys, Do not track?, Ad Block Enabled, Canvas Rendering, Prefered Language, GPU Images, Writing Sysem Supported?, Fonts (javascript), Writitng System Display, Video".split(",")
+  All="agent, timezone, resolution, fontlist, plugins, cookie, localstorage, accept, encoding, headerkeys, dnt, adBlock, canvastest, language, hashes, langs, fonts, lang_hash, video".replace(" ", "").split(",")
+  Cross_Browser="ratio, langs, audio, timezone, accept, hashes".replace(" ", "").split(",")
+  #Cross_Browser="fonts".replace(" ", "").split(",")
+  Single_Browser=All
+  Amiunique="agent, timezone, resolution, plugins, cookie, localstorage, accept, encoding, language, headerkeys, dnt, adBlock, canvastest".replace(" ", "").split(",")
+  CB_Amiunique="accept, timezone, ratio, localstorage".replace(" ", "").split(",")
+
+def read_file(name):
+  with open(name, "r") as file:
+    return file.read()
+
+class Masks(Enum):
+  try :
+    GPU = loads(read_file("GPU_Mask.txt"))
+    Lang = loads(read_file("Lang_Mask.txt"))
+    Font = loads(read_file("Font_Mask.txt"))
+  except:
+    pass
 
 class Fingerprint_Type(Enum):
   CROSS = 0
   SINGLE = 1
 
+def masked_array(array, mask):
+  return [e for i, e in enumerate(array) if mask[i]]
+
 class Fingerprint_Base:
   def __init__(self):
     self.fp = None
-
-  def __eq__(self, other):
-    return self.fp == other.fp
-
-  def __ne__(self, other):
-    return not self.__eq__(other)
-
-  def __hash__(self):
-    return hash(self.fp)
-
-  def __str__(self):
-    if isinstance(self.fp, list):
-      return ", ".join(str(x) for x in self.fp)
-    else:
-      return str(self.fp)
-
-  def __format__(self, format_spec):
-    return format(str(self), format_spec)
-
-class GPU_Fingerprint(Fingerprint_Base):
-  #Data is array of gpu hashes up to video
-  def __init__(self, data, fp_type, valid):
-    Fingerprint_Base.__init__(self)
-    if fp_type == Fingerprint_Type.CROSS:
-      self.valid = valid
-
-      if valid:
-        cross_mask = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0]
-        self.fp = ""
-        for i, h in enumerate(data):
-          if cross_mask[i]:
-            self.fp += str(h)
-    else:
-      self.valid = True
-      self.fp = "".join(data)
+    self.valid = True
 
   def __eq__(self, other):
     if self.valid and other.valid:
@@ -52,26 +43,65 @@ class GPU_Fingerprint(Fingerprint_Base):
     else:
       return True
 
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
   def __hash__(self):
     if self.valid:
-      return hash(self.fp)
+      return hash("".join(str(x) for x in self.fp))
     else:
-      return hash(1.6180339887498948482*1e10)
+      return hash(None)
 
-class Lang_Fingerprint(Fingerprint_Base):
-  #Data is string of if the lang can be displayed or not
-  def __init__(self, data, fp_type):
+  def __str__(self):
+    return str(self.fp)
+
+  def __format__(self, format_spec):
+    return format(str(self), format_spec)
+
+class GPU_Fingerprint(Fingerprint_Base):
+  #Data is array of gpu hashes up to video
+  def __init__(self, data, fp_type, valid, browser, b2, masks=""):
     Fingerprint_Base.__init__(self)
-    cross_mask = [1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0]
     if fp_type == Fingerprint_Type.CROSS:
-      self.fp = ""
-      for i, l in enumerate(list(data)):
-        if cross_mask[i]:
-          self.fp += str(l)
+      self.valid = valid
+      if valid:
+        try:
+          if masks == "":
+            mask = Masks.GPU["{}{}".format(browser, b2)]
+          else:
+            mask = masks
+        except:
+          self.valid = False
+          self.fp = None
+          return
+
+        self.fp = []
+        for i, h in enumerate(data):
+            if mask[i]:
+              self.fp.append(h)
     else:
       self.fp = data
 
-class Video_Fingerprint(Fingerprint_Base):
+
+class Lang_Fingerprint(Fingerprint_Base):
+  #Data is string of if the lang can be displayed or not
+  def __init__(self, data, fp_type, browser, b2):
+    Fingerprint_Base.__init__(self)
+    if fp_type == Fingerprint_Type.CROSS:
+      try:
+        mask = Masks.Lang["{}{}".format(browser, b2)]
+      except:
+        self.valid = False
+        self.fp = None
+        return
+      self.fp = []
+      for i, h in enumerate(data):
+        if mask[i]:
+          self.fp.append(h)
+    else:
+      self.fp = data
+
+class Video_Fingerprint():
   # Data is array of video hash codes
   def __init__(self, data):
     self.valid = (data[0] != 'No video')
@@ -89,6 +119,9 @@ class Video_Fingerprint(Fingerprint_Base):
     else:
       return len(self.ctx & other.ctx) != 0 and len(self.gl & other.gl) != 0
 
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
   def __hash__(self):
     if self.valid:
       return hash(1.6180339887498948482*1e10)
@@ -99,32 +132,68 @@ class Video_Fingerprint(Fingerprint_Base):
     return "{} {}".format(self.ctx, self.gl)
 
 class Font_Fingerprint(Fingerprint_Base):
-  def __init__(self, data, fp_type):
-    cross_mask = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1]
-    if fp_type == Fingerprint_Type.CROSS:
-      self.fp = ""
-      for i, font in enumerate(data):
-        if cross_mask[i]:
-          self.fp += font
-    else:
-      self.fp = "".join(data)
-
-class Feature_Fingerprint(Fingerprint_Base):
-  def __init__(self, data):
+  def __init__(self, data, fp_type, browser, b2):
     Fingerprint_Base.__init__(self)
+    if fp_type == Fingerprint_Type.CROSS:
+      try:
+        mask = Masks.Font["{}{}".format(browser, b2)]
+      except:
+        mask = [1] * len(data);
+
+      self.fp = []
+      for i, h in enumerate(data):
+        if mask[i]:
+          self.fp.append(h)
+    else:
+      self.fp = data
+
+class Core_Fingerprint():
+  def __init__(self, data):
     self.fp = data
 
-class Fingerprint(Fingerprint_Base):
+  def __eq__(self, other):
+    return self.fp == other.fp
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __hash__(self):
+    return hash(self.fp)
+
+  def __str__(self):
+    return str(self.fp)
+
+
+    
+
+class Feature_Fingerprint():
+  def __init__(self, data):
+    self.fp = data
+
+  def __eq__(self, other):
+    return self.fp == other.fp
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __hash__(self):
+    return hash(self.fp)
+
+  def __str__(self):
+    return str(self.fp)
+
+class Fingerprint():
   # to_add_attrs a list or string
-  def __init__(self, cursor, image_id, table_name, fp_type, to_add_attrs):
-    Fingerprint_Base.__init__(self)
-    self.cursor, self.image_id, self.table_name, self.fp_type = cursor, image_id, table_name, fp_type
+  def __init__(self, cursor, image_id, table_name, fp_type, to_add_attrs, b2=None, masks=""):
+    self.cursor, self.image_id, self.table_name, self.fp_type, self.b2,self.masks = cursor, image_id, table_name, fp_type, b2, masks
+    if fp_type == Fingerprint_Type.CROSS:
+      assert(b2 is not None)
 
     self.attrs = Set()
     self.fp = []
     self.cursor.execute("SELECT gpu, browser from {} where image_id='{}'".format(table_name, image_id))
-    gpu, browser = cursor.fetchone()
-    self.software_render = gpu == "SwiftShader" or browser == "Edge"
+    gpu, self.browser = cursor.fetchone()
+    self.software_render = (gpu == "SwiftShader" or gpu == "Microsoft Basic Render Driver")
     if isinstance(to_add_attrs, list):
       for attr in to_add_attrs:
         self.__add_attr(attr)
@@ -140,26 +209,53 @@ class Fingerprint(Fingerprint_Base):
 
       if attr == 'langs':
         self.fp.append(
-          Lang_Fingerprint(data, self.fp_type)
+          Lang_Fingerprint(data, self.fp_type, self.browser, self.b2)
         )
       elif attr == 'hashes':
         hashes = data.split("&")
         self.fp.append(
-          GPU_Fingerprint(hashes[:27], self.fp_type, self.software_render)
+          GPU_Fingerprint(hashes[:27], self.fp_type, not self.software_render, self.browser, self.b2, masks=self.masks)
         )
       elif attr == 'fonts':
         self.fp.append(
-          Font_Fingerprint(list(data), self.fp_type)
+          Font_Fingerprint(list(data), self.fp_type, self.browser, self.b2)
         )
       elif attr == 'video':
         hashes = data.split("&")
         self.fp.append(
           Video_Fingerprint(hashes)
         )
+      elif attr == 'audio':
+        if self.browser == 'IE' or self.b2 == 'IE':
+            self.fp.append('not supported')
+        else:
+          self.fp.append(
+            Feature_Fingerprint(data.split('_')[0])
+          )
+
       else:
         self.fp.append(
           Feature_Fingerprint(data)
         )
 
   def __hash__(self):
-    return hash("".join(str(x) for x in self.fp))
+    hash_code = hash(None)
+    for fp in self.fp:
+      hash_code ^= hash((hash_code, fp))
+
+    return hash_code
+
+  def __eq__(self, other):
+    return self.fp == other.fp
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __str__(self):
+    if isinstance(self.fp, list):
+      return ", ".join(str(x) for x in self.fp)
+    else:
+      return str(self.fp)
+
+  def __format__(self, format_spec):
+    return format(str(self), format_spec)
