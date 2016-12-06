ad5417b17114f5c32d2c7eee281c98bad722f5de
songli <993793072@qq.com>
2016-12-06 22:06:41 +0000

    updated server

--- flask/server.py
+++ flask/server.py
@@ -13,14 +13,60 @@ def hello():
 
 @app.route('/features', methods=['POST'])
 def features():
-    feature_list = ["fontlist", "WebGL", "inc", "gpu", "hash", "timezone", "plugins", "cookie", "localstorage", "gpuImgs", "adBlock", "cpu_cores", "canvas_test", "audio"]
+    agent = ""
+    accept = ""
+    encoding = ""
+    language = ""
+
+    try:
+        agent = request.headers.get('User-Agent')
+        accpet = request.headers.get('Accept')
+        encoding = request.headers.get('Accept-Encoding')
+        language = request.headers.get('Accept-Language')
+    except:
+        pass
+
+    print agent, accept, encoding, language
+
+    feature_list = [
+            #"fontlist", 
+            "agent",
+            "accept",
+            "encoding",
+            "language",
+            "fonts",
+            "WebGL", 
+            "inc", 
+            "gpu", 
+            "hash", 
+            "timezone", 
+            "plugins", 
+            "cookie", 
+            "localstorage", 
+            "adBlock", 
+            "cpu_cores", 
+            "canvas_test", 
+            "audio"]
+
+    cross_feature_list = [
+            "timezone",
+            "fonts"
+            ]
+
     single_hash = ""
+    print single_hash
 
     result = request.get_json()
+    result['agent'] = agent
+    result['accept'] = accept
+    result['encoding'] = encoding
+    result['language'] = language
    # print (request.form)
     
     for feature in feature_list:
         single_hash += str(result[feature])
+        hash_object = hashlib.md5(str(result[feature]))
+        print feature, hash_object.hexdigest()
 #        print str(result[feature])
 
     hash_object = hashlib.md5(single_hash)
