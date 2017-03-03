66ce0dce6c3f6136d6c6471d2a8328f46062f2ce
Song-Li <lisong1013@gmail.com>
2017-03-03 16:26:14 -0500

    removed some useless files

--- restart.sh
+++
@@ -1,4 +0,0 @@
-python wipe_table.py cross_browser new_data uid;
-find /home/site/data/images/origins/ -name "*.*" -delete
-sudo rm -r /home/site/data/images/generated/* ;
-sudo /etc/init.d/apache2 restart
