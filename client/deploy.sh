bda72c8935b9acd458573cee50b48a648b963c1d
sol315 <lisong1013@gmail.com>
2017-04-01 18:03:02 -0400

    changed to www.songli.us

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,3 +1,4 @@
+rm -r ~/projs/test_site/*
 cp -rf ./* ~/projs/test_site/;
 cd ~/projs/test_site/ ;
 git add -A;
--- client/fingerprint/js/toServer.js
+++ client/fingerprint/js/toServer.js
@@ -281,7 +281,7 @@ var Sender = function() {
           data : JSON.stringify(postData),
           success : function(data) {
             console.log(data);
-            parent.postMessage(data,"http://127.0.0.1:9876");
+            parent.postMessage(data,"http://www.songli.us");
             //parent.postMessage(data,"http://uniquemachine.org");
           },
           error: function (xhr, ajaxOptions, thrownError) {
