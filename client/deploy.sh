cc0438a73ff2662d301a65fb97229c997e4efc54
sol315 <lisong1013@gmail.com>
2017-04-01 17:58:38 -0400

    added to test_site

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,5 +1,5 @@
-cp -rf ./* ~/projs/uniquemachine.github.io/;
-cd ~/projs/uniquemachine.github.io/ ;
+cp -rf ./* ~/projs/test_site/;
+cd ~/projs/test_site/ ;
 git add -A;
 git commit -m "new";
 git push -u origin master;
