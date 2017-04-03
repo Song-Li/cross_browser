c3d03fc4673314a6026efa9d54ddafa537ddd8b5
sol315 <lisong1013@gmail.com>
2017-04-03 17:09:45 -0400

    show first column of pictures

--- client/result/js/get_result.js
+++ client/result/js/get_result.js
@@ -1,4 +1,5 @@
 ip_address = "aws.songli.us/uniquemachine"
+address = "http://aws.songli.us/pictures/"
 function send_to_utils(command) {
   res = ""
   $.ajax({
@@ -17,18 +18,34 @@ function send_to_utils(command) {
   return res; 
 }
 
-function get_ips() {
-  ips = send_to_utils("keys").split(',');
-  return ips;
+function get_keys() {
+  keys = send_to_utils("keys").split(',');
+  return keys;
 }
 
 function get_result() {
-  var ips = get_ips();
-  console.log(ips);
-  for (var idx in ips) {
-    console.log(ips[idx]);
-    ip = ips[idx];
-    var b = $('<input type="button" value = ' + ip + ' />');
+  var keys = get_keys();
+  console.log(keys);
+  for (var idx in keys) {
+    id = keys[idx].split('_')[1];
+    ip = keys[idx].split('_')[0];
+
+    var b = $('<input type="button" value = ' + ip + ' onclick="get_pictures_by_id(' + id + ')"/>');
     $("#keys").append(b);
+  }
+}
+
+function get_pictures_by_id(id) {
+  console.log(id);
+  picture_ids = send_to_utils("get_pictures_by_id," + id).split(',');
+  sorted = {}
+  for(var idx in picture_ids) {
+    v = picture_ids[idx]
+    sorted[parseInt(v.split('_')[0])] = v.split('_')[1]
+  }
+  for(k in sorted){
+    console.log(k)
+    var img = $('<img src="' + address + sorted[k] + '.png" />');
+    $('#picture_1').append(img);
   }
 }
