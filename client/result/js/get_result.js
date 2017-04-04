7dc520a381e33713de0e15fe5010fc2fed608053
sol315 <lisong1013@gmail.com>
2017-04-04 14:15:00 -0400

    result v0.5 finished

--- client/result/js/get_result.js
+++ client/result/js/get_result.js
@@ -69,19 +69,74 @@ function get_pictures_by_id(column, id) {
 // this function is used to get the pixels of a img
 // var: img html object
 // return: array of pixels
-function get_pixel_from_img(img) {
+function get_pixel_from_img(img_id) {
+  var img = document.getElementById(img_id); 
   var width = img.clientWidth;
   var height = img.clientHeight;
+  console.log(width, height);
   // the canvas may be taint for cross origin info
   var canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
   var context = canvas.getContext('2d');
   context.drawImage(img, 0, 0);
   data = context.getImageData(0, 0, width, height);
+  delete canvas;
   return data; 
 }
 
+
+function sub_pic_data(pic1_data, pic2_data) {
+  var sub_data = pic1_data;
+  var same = true;
+  console.log(pic1_data.data.length)
+  for (var i in pic1_data.data) {
+    // both the two will be considered
+    sub_data.data[i] = Math.abs(pic1_data.data[i] - pic2_data.data[i]);
+
+    // here we make the pixels 50 times brighter
+    // may overflow, but seems ok
+    sub_data.data[i] *= 50;
+
+    // force set every alpha value to be 255
+    if (i % 4 == 3) {
+      sub_data.data[i] = 255;
+    }else if (sub_data.data[i] != 0) {
+      same = false;
+    }
+  }
+  sub_data.same = same;
+  console.log(sub_data);
+  return sub_data;
+}
+
+// subtract all the imgs
 function subtract() {
-  pic1_data = get_pixel_from_img(document.getElementById('10'));
-  console.log(pic1_data); 
-  console.log("here");
+  // clear the res div
+  $('#subtract').empty();
+  // here we only have 28 pictures
+  for (var idx = 0;idx < 28;++ idx) {
+    pic1_data = get_pixel_from_img("1" + idx);
+    pic2_data = get_pixel_from_img("2" + idx);
+    var sub_data = sub_pic_data(pic1_data, pic2_data);
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = sub_data.width;
+    canvas.height = sub_data.height;
+    ctx.putImageData(sub_data, 0, 0);
+    $('#subtract').append(canvas);
+
+    var res_img = new Image(); 
+    res_img.height = "32";
+    res_img.width = "32";
+    res_img.align = "top";
+    if (sub_data.same) {
+      res_img.src = "./img/yes.png";
+    } else {
+      res_img.src = "./img/no.png";
+    } 
+
+    $('#subtract').append(res_img);
+  }
 }
