c75d8cac287210f3749995d64f68eee9f3cd68db
sol315 <lisong1013@gmail.com>
2017-04-04 12:15:31 -0400

    make it able to get pixels

--- client/result/js/get_result.js
+++ client/result/js/get_result.js
@@ -1,5 +1,6 @@
 ip_address = "aws.songli.us/uniquemachine"
 address = "http://aws.songli.us/pictures/"
+// send information to utils function
 function send_to_utils(command) {
   res = ""
   $.ajax({
@@ -18,11 +19,13 @@ function send_to_utils(command) {
   return res; 
 }
 
+// get all of the tests
 function get_keys() {
   keys = send_to_utils("keys").split(',');
   return keys;
 }
 
+// gengerate keys
 function get_result() {
   var keys = get_keys();
   console.log(keys);
@@ -42,19 +45,43 @@ function get_pictures(column) {
   get_pictures_by_id(column, id);
 }
 
+// add picture to html
 function get_pictures_by_id(column, id) {
+  // clear this div
+  $('#picture_' + column).empty();
+
   picture_ids = send_to_utils("get_pictures_by_id," + id).split(',');
   sorted = {}
   for(var idx in picture_ids) {
     v = picture_ids[idx]
-    sorted[parseInt(v.split('_')[0])] = v.split('_')[1]
+      sorted[parseInt(v.split('_')[0])] = v.split('_')[1]
   }
   for(k in sorted){
-    var img = $('<img id=' + column + k + ' src="' + address + sorted[k] + '.png" />');
+    var img = new Image();
+    // the cross origin cliam have to before src attrabute
+    img.crossOrigin="Anonymous";
+    img.id = column + k;
+    img.src = address + sorted[k] + '.png';
     $('#picture_' + column).append(img);
   }
 }
 
+// this function is used to get the pixels of a img
+// var: img html object
+// return: array of pixels
+function get_pixel_from_img(img) {
+  var width = img.clientWidth;
+  var height = img.clientHeight;
+  // the canvas may be taint for cross origin info
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+  context.drawImage(img, 0, 0);
+  data = context.getImageData(0, 0, width, height);
+  return data; 
+}
+
 function subtract() {
+  pic1_data = get_pixel_from_img(document.getElementById('10'));
+  console.log(pic1_data); 
   console.log("here");
 }
